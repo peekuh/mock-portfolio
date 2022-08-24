7024fdc5be67ffd09c7736d5444d6ece200a3343
Srijan sarthak <sar.srijan@gmail.com>
2022-08-24 23:33:39 +0530

    hamburger to cross, navigation close on section selection

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-alert("This is some text");
+
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
